084b9dea65d78337092984647c3a617ff658a4fd
Darosss <daroing12@gmail.com>
2023-10-06 11:48:56 +0200

    refactor: replace hardocded url with variables from configs

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,6 +5,7 @@ import { OrderController } from './order.controller';
 import { Order } from './entities/order.entity';
 import { OrderService } from './order.service';
 import { PRODUCT_SERVICE_NAME, PRODUCT_PACKAGE_NAME } from './pb/product.pb';
+import { PRODUCT_MICROSERVICE_URL } from 'src/configs';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { PRODUCT_SERVICE_NAME, PRODUCT_PACKAGE_NAME } from './pb/product.pb';
         name: PRODUCT_SERVICE_NAME,
         transport: Transport.GRPC,
         options: {
-          url: '0.0.0.0:50053',
+          url: PRODUCT_MICROSERVICE_URL,
           package: PRODUCT_PACKAGE_NAME,
           protoPath: 'node_modules/e-shop-nest-proto/proto/product.proto',
         },
